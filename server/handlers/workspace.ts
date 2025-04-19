74272b570f6e6ae014659debab47687e31037b59
Adam Comer <adambcomer@proton.me>
2025-04-19 04:12:05 -0600

    Replaced deprecated send

--- server/handlers/workspace.ts
+++ server/handlers/workspace.ts
@@ -38,7 +38,7 @@ router.post('/', async (_, res) => {
 router.get('/:id', async (req, res) => {
   const workspace = await getWorkspace(db, req.params.id)
   if (!workspace) {
-    res.send(404)
+    res.sendStatus(404)
     return
   }
 
@@ -54,17 +54,17 @@ type WorkspacePutReqest = Request<
 router.put('/:id', async (req: WorkspacePutReqest, res) => {
   const ruleset: unknown = req.body.ruleset
   if (!isRuleSet(ruleset)) {
-    res.send(422)
+    res.sendStatus(422)
     return
   }
 
   const sentence: unknown = req.body.sentence
   if (!isSentence(sentence)) {
-    res.send(422)
+    res.sendStatus(422)
     return
   }
 
   await updateWorkspace(db, req.params.id, ruleset, sentence)
 
-  res.status(200).send()
+  res.sendStatus(200)
 })
