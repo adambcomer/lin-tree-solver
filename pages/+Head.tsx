45ee63e5f249d18c11fa83ffba14e61bff036239
Adam Comer <adambcomer@proton.me>
2025-05-02 00:22:12 -0600

    Fixed font links to use Cloudflare Fonts

--- pages/+Head.tsx
+++ pages/+Head.tsx
@@ -17,6 +17,20 @@
 export const Head = () => {
   return (
     <>
+      <link rel='preconnect' href='https://fonts.googleapis.com' />
+      <link
+        rel='preconnect'
+        href='https://fonts.gstatic.com'
+        crossOrigin='anonymous'
+      />
+      <link
+        href='https://fonts.googleapis.com/css2?family=Noto+Sans+Mono:wght@100..900&family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap'
+        rel='stylesheet'
+      />
+      <link
+        href='https://fonts.googleapis.com/css2?family=Noto+Sans+Mono:wght@100..900&family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap'
+        rel='stylesheet'
+      />
       {import.meta.env.PROD && (
         <script
           defer
